8b8f001103bfdae173d5ece366e6578df9744454
Lauren Tsao <et2676@nyu.edu>
2024-09-26 18:01:15 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -27,7 +27,7 @@ function setup() {
     arc(x, 0, 160, 160, 0, 90);
   }
 
-  // draw a row of 02_square
+  // 02_square
   for (let x = 80; x < width; x += 640) {
     noStroke();
     fill(120, 120, 120);
@@ -41,25 +41,32 @@ function setup() {
     rect(x, 80, 160, 80);
   }
 
-  // 03_rect row triangle
+  // 03_rect row - triangle
   for (let x = 0; x < width; x += 640) {
     noStroke();
     fill(200, 200, 200);
     triangle03(x, 160, 160, 80, 0);
   }
 
-  // draw a row of 04_square
+  // 04_square
   for (let x = 160; x < width; x += 320) {
     noStroke();
     fill(150, 150, 150);
     square(x, 0, 160);
   }
 
-  // draw a row of 04_circle
+  // 04_square row - circle
   for (let x = 240; x < width; x += 640) {
     noStroke();
     fill(200, 200, 200);
     circle(x, 80, 160);
+  }
+
+  //04_square row - semi-circle
+  for (let x = 480; x < width; x += 640) {
+    noStroke();
+    fill(200, 200, 200);
+    arc(x, 80, 160, 160, 270, 450);
   }
 
   // 05_square row
@@ -69,33 +76,34 @@ function setup() {
     square(x, 0, 80);
   }
 
-  // draw a row of 06_square
+  // 06_square row
   for (let x = 400; x < width; x += 640) {
     noStroke();
     fill(120, 120, 120);
     square(x, 0, 80);
   }
 
-  // 06_square row triangle
+  // 06_square row - triangle
   for (let x = 400; x < width; x += 640) {
     noStroke();
     fill(220, 0, 200);
     triangle03(x, 0, 80, 40, 45, 1.4);
   }
 
-  // draw a row of 07_square
+  // 07_square row
   for (let x = 320; x < width; x += 640) {
     noStroke();
     fill(100, 100, 100);
     square(x, 80, 80);
   }
 
-  // draw a row of 08_square
+  // 08_square row
   for (let x = 400; x < width; x += 640) {
     noStroke();
     fill(170, 170, 170);
     square(x, 80, 80);
   }
+
 }
 
 function draw() {}
